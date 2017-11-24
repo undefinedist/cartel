6ac5593330726b6872270ad069f4cedb2976d206
Eung Jin Lee <oiojin831@gmail.com>
2017-11-24 11:47:57 +0900

    add disqus

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,36 +2,44 @@ import React from 'react'
 import Link from 'gatsby-link'
 import Header from '../components/sicario/Header'
 import Fixed from '../components/sicario/Fixed'
+import ReactDisqusThread from 'react-disqus-thread'
 
 const IndexPage = ({data}) => {
   const header = data.allContentfulPartner.edges[0].node.header
   console.log(header)
 
   return (
-    <Fixed top right left z={100}>
-      <Header
-        bg="blue"
-        text="예약하기"
-        logoPx={[5]}
-        src={`https://dummyimage.com/300x82/000/fff.png&text=${
-          header.companyName
-        }`}
-        logoWrapper={{
-          px: [1, 1, 1, 2],
-          py: [1, 1, 1, 1],
-        }}
-        btnWrapper={{
-          px: [1, 1, 1, 2],
-          py: [1, 1, 1, 1],
-        }}
-        btn={{
-          fontSize: [1],
-          color: header.btnColor,
-          bg: header.btnBg,
-          px: header.btnPx,
-        }}
+    <div>
+      <Fixed top right left z={100}>
+        <Header
+          bg="blue"
+          text="예약하기"
+          logoPx={[5]}
+          src={`https://dummyimage.com/300x82/000/fff.png&text=${
+            header.companyName
+          }`}
+          logoWrapper={{
+            px: [1, 1, 1, 2],
+            py: [2, 2, 2, 1],
+          }}
+          btnWrapper={{
+            px: [1, 1, 1, 2],
+            py: [1, 1, 1, 1],
+          }}
+          btn={{
+            fontSize: [1],
+            color: header.btnColor,
+            bg: header.btnBg,
+            px: header.btnPx,
+          }}
+        />
+      </Fixed>
+      <ReactDisqusThread
+        style={{marginTop: '100px'}}
+        shortname="skywings"
+        title="Example Thread"
       />
-    </Fixed>
+    </div>
   )
 }
 
